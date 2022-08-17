a3879bbec40aaab91930054bdf6d5cc188b83c37
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:35:14 +0530

    *Check Password Using REGEX Pattern*

---
+++ PasswordRule4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+    class PasswordRule4
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[0-9]{1}['@'][A-Z a-z]{7}$";
+        public bool ValidatePasswordFour(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_PASSWORD);
+
+        }
+    }
+}
