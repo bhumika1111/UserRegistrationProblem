e78856df9150bc6b08296159ec17b5622a251b15
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:29:14 +0530

    *Check User Mobile Number Using REGEX Pattern*

---
+++ MobileFormate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+    class MobileFormate
+    {
+        public static string REGEX_MOBILENUMBER = "^[91]{2}[\\s][6-9]{1}[0-9]{9}$";
+        public bool ValidateMobileNumber(string MobileNumber)
+        {
+            return Regex.IsMatch(MobileNumber, REGEX_MOBILENUMBER);
+
+        }
+    }
+}
