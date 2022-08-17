aac033d1df1f05528024e9237ee4ae2b64a2d420
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:27:05 +0530

    *Check User Email Address Using REGEX Pattern*

---
+++ Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+    class Email
+    {
+        public static string REGEX_EMAIL = "^[a-z]{3}[0-9]{3}['@'][a-z]{5}['.'][a-z]{3}$";
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, REGEX_EMAIL);
+
+        }
+    }
+}
