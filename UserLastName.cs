f8556b30db57fbd5d85a9b8387ff475cd3456f17
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:24:55 +0530

    *Check User Last Name Using REGEX Pattern*

---
+++ UserLastName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+    class UserLastName
+    {
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2}$";
+        public bool ValidateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, REGEX_LASTNAME);
+
+        }
+    }
+}
+
