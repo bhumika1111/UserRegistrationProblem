53af2b27dd7d2a2231a8fe28bb257d4f3df3884d
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:22:48 +0530

    *Check User First Name Using REGEX Pattern*

---
+++ UserFirstName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+  public  class UserFirstName
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2}$";
+        public bool ValidateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, REGEX_FIRSTNAME);
+
+        }
+    }
+
+   
+}
