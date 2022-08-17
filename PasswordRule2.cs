a9d4e2f4b8a89f19ec273d23cd969039be6768a8
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:32:57 +0530

    *Check Password Using REGEX Pattern*

---
+++ PasswordRule2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+    class PasswordRule2
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[A-Z a-z]{7}$";
+        public bool ValidatePasswordTwo(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_PASSWORD);
+
+        }
+
+    }
+}
