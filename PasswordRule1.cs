05d13b24e9f84646e5841143414811227eb146cd
bhumika1111 <bhoyarpatil@434>
2022-08-17 15:31:52 +0530

    *Check Password Using REGEX Pattern*

---
+++ PasswordRule1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegixDemo
+{
+   public class PasswordRule1
+    {
+        public static string REGEX_PASSWORD = "^[A-Z a-z]{8}$";
+        public bool ValidatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_PASSWORD);
+
+        }
+
+    }
+}
